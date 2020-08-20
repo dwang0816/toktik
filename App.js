bb97ef509d57cb6fbe3b7df70b27bbd452a9f6fe
Dennis Wang <dwang0816@gmail.com>
2020-08-20 15:06:31 -0400

    set up interface

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Video from './Video'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="app">
+      <h1>This is the app</h1>
+      <div className="app_videos">
+        <Video/>
+        <Video/>
+        <Video/>
+        <Video/>
+      </div>
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <div className="video">
+      <h1>this is a video</h1>
+    </div>
+  )
+}
+
+export default Video
