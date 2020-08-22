4235a122b971867a21b7c2dcec8f36d7c460da79
Dennis Wang <dwang0816@gmail.com>
2020-08-21 21:34:55 -0400

    set up template for videos

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Video from './Video'
+import Video from './Video';
+import "./App.css";
 
 export default function App() {
   return (
--- Video.js
+++ Video.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./Video.css"
 
 function Video() {
   return (
     <div className="video">
       <h1>this is a video</h1>
+      {/* <VideoSideBar/> */}
     </div>
   )
 }
